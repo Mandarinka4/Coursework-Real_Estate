ba0c9e3e7c4316d212aac6c499f392c0709518f9
Maryna <maryna.osypova.oi.2022@lpnu.ua>
2024-12-04 01:01:38 +0200

    final commit

--- pages/index.js
+++ pages/index.js
@@ -90,6 +90,18 @@ const Home = ({ propertiesForSale, propertiesForRent }) => (
                 <Property property={property} key={property.id} />
             ))}
         </Flex>
+
+        <Box m="10" borderRadius="lg" overflow="hidden">
+            <iframe
+                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3151.835434509206!2d144.95373631531586!3d-37.81627997975153!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x6ad642af0f11fd81%3A0xf577bb5f6c0b5df5!2sMelbourne%20VIC%2C%20Australia!5e0!3m2!1sen!2sus!4v1637779110439!5m2!1sen!2sus"
+                width="100%"
+                height="450"
+                style={{ border: 0 }}
+                allowFullScreen=""
+                loading="lazy"
+            ></iframe>
+        </Box>
+
     </Box>
 );
 
